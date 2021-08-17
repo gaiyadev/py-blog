c8d868bae400e6b57e20a9552dd011898f134bf7
gaiyadev <gaiyaobed94@gmail.com>
2021-08-18 00:04:09 +0100

    status code done

--- post_api/models.py
+++ post_api/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 
 class Post (models.Model):
-    title = models.CharField(max_length=300)
-    body = models.TextField(max_length=6000)
-   # created_at = models.DateField(auto_created=True, blank=True, default=True)
+    title = models.CharField(max_length=300, unique=True)
+    body = models.TextField(max_length=600)
+    author = models.TextField(max_length=100, blank=True)
+    isPublish = models.BooleanField(default=False)
+    slug = models.SlugField(default="", blank=True,
+                            db_index=True, editable=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
--- post_api/views.py
+++ post_api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import serializers
+# from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
+
 # Create your views here.
+
+# Get all post
 
 
 @api_view(['GET'])
@@ -13,20 +16,34 @@ def index(request):
     try:
         allPosts = Post.objects.all()
         serializer = PostSerializer(allPosts, many=True)
-        return Response(serializer.data,)
+        return Response(serializer.data)
     except:
-        Response('Something went wrong', status=500)
+        Response('Something went wrong',
+                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# Create a new post
 
 
 @api_view(['POST'])
 def create(request):
     try:
+        title = request.data['title']
+        body = request.data['body']
+        isPublish = request.data['isPublish']
+        # check is post exist
+        poste = Post.objects.get(title=title)
+        if poste:
+            return Response('Post already exist', status=status.HTTP_409_CONFLICT)
+
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     except:
-        Response('Something went wrong', status=500)
+        Response('Something went wrong',
+                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# Update a new post
 
 
 @api_view(['PUT'])
@@ -36,9 +53,12 @@ def update(request, postId):
         serializer = PostSerializer(instance=post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     except:
-        Response('Something went wrong', status=500)
+        Response('Something went wrong',
+                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# Get a single post
 
 
 @api_view(['GET'])
@@ -48,7 +68,9 @@ def fetchPost(request, postId):
         serializer = PostSerializer(post, many=False)
         return Response(serializer.data,)
     except:
-        return Response('Post not found', status=404)
+        return Response('Post not found', status=status.HTTP_404_NOT_FOUND)
+
+# Delete a post
 
 
 @api_view(['DELETE'])
@@ -56,6 +78,6 @@ def deletePost(request, postId):
     try:
         post = Post.objects.get(id=postId)
         post.delete()
-        return Response(data='Post deleted', status=200)
+        return Response(data='Post deleted', status=status.HTTP_200_OK)
     except:
-        return Response('Post not found', status=404)
+        return Response('Post not found', status=status.HTTP_404_NOT_FOUND)
